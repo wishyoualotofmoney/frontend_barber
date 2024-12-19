26e08ac3b28a26c4b8e753d9be2ec0c8dbd82703
Азимут <98122726+wishyoualotofmoney@users.noreply.github.com>
2024-12-19 16:43:54 +0300

    текст в начальной странице

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './main/main.component'; 
 
 export const appRoutes: Routes = [
+  { path: '', component: MainComponent },   
   { path: '', component: AuthComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '' }
